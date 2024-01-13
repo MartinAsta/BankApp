95285e0af8f9a4039057ec85d42c16c9d012176d
Asta Martin <martin.asta@hotmail.com>
2024-01-13 11:56:40 +0100

    Started Blazor

--- obj/Debug/net8.0/BankApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/BankApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b52dfb2380bfb70ccd2fa09d668c46fc1271db90")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
